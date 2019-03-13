29644d5b7be4f4b23937b02d79d2b3488f4763f5
Edison <jianning.18@intl.zju.edu.cn>
2019-03-13 10:04:13 +0800

    mathematics
    a equals to e hahaha. Because b=a*1001, and then e=b/(7*11*13)=b/1001=a*1001/1001=a.

---
+++ Practical4/variables.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 09:53:47 2019
+
+@author: KANG Jianning
+"""
+
+a=140
+b=a*1000+a
+c=b/7
+d=c/11
+e=d/13
+print (e)
+print ('a equals to e hahaha')
+print ('Because b=a*1001, and then e=b/(7*11*13)=b/1001=a*1001/1001=a')
