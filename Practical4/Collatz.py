82f01f41c899dbaba0e84baf7641b42bd735c49f
Edison <jianning.18@intl.zju.edu.cn>
2019-03-13 11:15:53 +0800

    This is the file for calculating Collatz, finishing at 1 first appears.

---
+++ Practical4/Collatz.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 10:18:54 2019
+
+@author: KANG Jianning
+"""
+
+# Do the while-loop
+# First determine whether is an odd number or even number
+# Then do the calculate
+# Finally determine whether it equals to 1
+
+n=25
+while 1==1:
+    if n%2==0:
+        n=n/2
+        print (n)
+    elif n==1:
+        break
+        print (n)
+    elif n%2==1:
+        n=n*3+1
+        print (n)
