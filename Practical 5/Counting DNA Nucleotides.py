9391cc1e12622510d56e1846a3b6e15e7577902b
Edison <jianning.18@intl.zju.edu.cn>
2019-03-21 10:56:49 +0800

    Shorten and improve the codes.

--- Practical 5/Counting DNA Nucleotides.py
+++ Practical 5/Counting DNA Nucleotides.py
@@ -6,25 +6,22 @@ Created on Wed Mar 20 09:09:37 2019
 """
 
 a= input('Give me a sequence of DNA:')
-b=0
-c=0
-d=0
-e=0
-for char in a:
-    if char == 'A' :
-        b += 1
-    if char == 'C':
-        c += 1
-    if char == 'G' :
-        d += 1
-    if char == 'T' :
-        e += 1
-print ("A':", b,',',"'C':", c,',',"'G':", d,',',"'T':", e,'.')
+b= list(a)
+myDict={}
+for word in a:
+    if word in ['A', 'T', 'G', 'C']:
+        if word in myDict:
+            myDict[word] += 1
+        else:
+            myDict[word] = 1
+myDict
+print (myDict)
+        
 
 import matplotlib.pyplot as plt
-labels = 'A', 'C', 'G', 'T'
-sizes = [b, c, d, e]
-explode = (0, 0.1, 0, 0) 
+labels = myDict.keys()
+sizes = myDict.values()
+explode = [0]*len(labels) 
 
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
@@ -32,3 +29,7 @@ ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
 ax1.axis('equal')  
 plt.title('pie of the four DNA nucleotides')
 plt.show()
+
+
+#labels = myDict.keys()
+#sizes = myDict.values()
