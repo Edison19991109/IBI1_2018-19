297eb578a20e7c61c08f18582c55494d09051b2b
Edison <jianning.18@intl.zju.edu.cn>
2019-05-05 16:48:15 +0800

    Finally fixed the error 'list out of range'.

--- Practical8/find_go_terms.py
+++ Practical8/find_go_terms.py
@@ -6,24 +6,24 @@ Created on Wed Apr 10 09:55:31 2019
 """
 
 import re
-from xml.dom.minidom import parse
+import xml.dom.minidom 
 import pandas as pd
 
 def countchildnodes(id, ChildrenSet):
     for gene in terms:
-        parent=gene.getElementsByTagName('is_a')[0].childNodes[0].data
+        parent=gene.getElementsByTagName('is_a')
         child=gene.getElementsByTagName('id')[0].childNodes[0].data
         for par in parent:
-            if par == id:
+            if par.childNodes[0].data == id:
                 ChildrenSet.add(child)
                 countchildnodes(child, ChildrenSet)
             
-
 df = pd.DataFrame(columns=['id','name','definition','childnodes'])   
-DomTree = parse('go_obo.xml')
+DomTree = xml.dom.minidom.parse('go_obo.xml')
 terms=DomTree.documentElement.getElementsByTagName('term')
 #extract the <is-a> tag, and then compare with gold term in the follwoing.
 #define a function and ilteration it in the set to find a child of 
+
 for term in terms:
     definition=term.getElementsByTagName('defstr')[0].childNodes[0].data
     if re.search('autophagosome',definition):
@@ -34,4 +34,3 @@ for term in terms:
         df = df.append(pd.DataFrame({'id':[id],'name':[name],'definition':[definition],'childnodes':[len(ChildrenSet)]}))
 
 df.to_excel('output.xlsx',index=False)  
-
