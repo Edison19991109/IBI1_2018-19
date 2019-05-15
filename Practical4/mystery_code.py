481b4b244a27c0f7e82d23b6eac1fe16f3ed1cc9
Edison <jianning.18@intl.zju.edu.cn>
2019-05-15 22:57:11 +0800

    Add more nodes in the file to explain more clearly.

--- Practical4/mystery_code.py
+++ Practical4/mystery_code.py
@@ -1,5 +1,5 @@
 # What does this piece of code do?
-# Answer: to get a prime number.
+# Answer: this piece of code is used to get a prime number.
 
 # Import libraries
 # randint allows drawing a random number, 
@@ -10,19 +10,30 @@ from random import randint
 # e.g. ceil(4.2)=5
 from math import ceil
 
+#Use a variable to control the loop. If it is 'False', it means that it has not found a prime number yet.
+#If it equals to 'True', then it suggests that it has got a prime number.
 p=False
+
+#Use a loop to try until it gets a prime number.
 while p==False:
+    
+    #If it finds a prime number, it won't change the value of p from 'True' to 'False'.
+    #Then it will break from the loop.
     p=True
-    #try to get a random number greater than 1 but less than 100
+    
+    #Try to get a random number greater than 1 but less than 100.
     n = randint(1,100)
+    
+    #To get the ceiling of suqare root of that random number.
+    #According to mathmatics, if there is not a factor after trying to suqare root of a number, then this number is a prime number.
     u = ceil(n**(0.5))
     for i in range(2,u+1):
-        # to see whether it has factor
-        # try until the ceiling number of the square number. 
-        # if it still doesn't have a factor, it means it's a prime number
+        # To see whether it has factor
+        # Try until the ceiling number of the square number. 
+        # If it still doesn't have a factor, it means it's a prime number
         if n%i == 0:
             p=False
 
 
-     
+#Then print out the number it finds.
 print(n)
